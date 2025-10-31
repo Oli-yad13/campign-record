5b3d8a66f28f89a7ec5d3492c2bbfd0f61758013
oli-yad13 <oliyadbekele.0@gmail.com>
2025-10-31 07:47:02 +0300

    Update route types import path in TypeScript definitions for consistency with build structure

---
+++ src/data/regions.ts
@@ -0,0 +1,30 @@
+export const ETHIOPIA_REGIONS = [
+  'Addis Ababa',
+  'Afar',
+  'Amhara',
+  'Benishangul-Gumuz',
+  'Dire Dawa',
+  'Gambela',
+  'Harari',
+  'Oromia',
+  'Sidama',
+  'SNNPR',
+  'Somali',
+  'South West Ethiopia Peoples',
+  'Tigray'
+] as const;
+
+export const ADDIS_SUB_CITIES = [
+  'Addis Ketema',
+  'Akaky Kaliti',
+  'Arada',
+  'Bole',
+  'Gullele',
+  'Kirkos',
+  'Kolfe Keranio',
+  'Lideta',
+  'Nifas Silk-Lafto',
+  'Yeka'
+] as const;
+
+
